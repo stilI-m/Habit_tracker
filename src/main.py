69781a3c21d6532e67c3597f39b6d2afac1264f1
stilI-m <henrybolton2kzz@gmail.com>
2026-06-22 12:59:34 +0300

    refactor: implement application factory pattern in main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.config import get_settings
+from src.config import get_settings, Settings
 from src.database.database import engine
 from src.database.models import Base
 from src.api.habits import router as habits_router
@@ -16,9 +16,16 @@ async def lifespan(app: FastAPI):
     yield
 
     await engine.dispose()
-app = FastAPI(title= settings.project_name, lifespan=lifespan)
-app.include_router(habits_router, prefix="/api/v1")
-@app.get("/ping")
-async def ping():
-    return {"status": "OK", "project": settings.project_name}
+def create_app(settings: Settings | None = None) -> FastAPI:
+    """Фабрика по сборке приложения."""
+    cfg = settings or get_settings()
 
+    app = FastAPI(
+        title=cfg.project_name,
+        lifespan=lifespan
+    )
+    app.include_router(habits_router, prefix="/api/v1")
+    @app.get("/ping")
+    async def ping():
+        return {"status": "OK", "project": settings.project_name}
+app = create_app()
